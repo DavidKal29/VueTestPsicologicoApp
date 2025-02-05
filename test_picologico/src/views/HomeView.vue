600e55bc63412b445bad89806fa770b62ca61536
David Kalmuk <correo@correo.com>
2025-02-05 21:58:58 +0100

    Comenzamos a desarrollar el home

--- test_picologico/src/views/HomeView.vue
+++ test_picologico/src/views/HomeView.vue
@@ -1,5 +1,25 @@
 <template>
-  <section></section>
+  <section class="">
+    <div class="">
+        <h1>BIENVENIDO AL</h1>
+        <h1>HUMANÓMETRO</h1>
+    </div>
+    <p>¡Prepárate!Vamos a evaluar tu calidad de humano a través de 5 preguntas. Ideal para reflexionar y compatir con amigos.</p>
+    <button class="">Comenzar</button>
+    <div class="">
+        <img src="../imagenes/Line 1.png" alt="">
+        <img src="../imagenes/Line 1.png" alt="">
+        <img src="../imagenes/Line 1.png" alt="">
+        <img src="../imagenes/Line 1.png" alt="">
+        <img src="../imagenes/Line 1.png" alt="">
+    </div>
+
+    <div class="">
+        <button class=""><i class="fa-solid fa-user"></i></button>
+        <button class=""><i class="fa-solid fa-right-from-bracket"></i></button>
+        <button class=""><i class="fa-solid fa-house"></i></button>
+    </div>
+  </section>
 </template>
 
 <script>
